c950703a23caf89e1adcd752621425b55458099e
priyans619 <priyansh.2b@gmail.com>
2024-11-21 12:44:43 +0530

    pass data to Header and ProductGrid component

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,23 @@
+import { useState } from "react";
 import Header from "@/components/Header";
+import ProductGrid from "@/components/ProductGrid";
 
 export default function Home() {
+  const [selectedCategory, setSelectedCategory] = useState("");
+  const [selectedSubCategory, setSelectedSubCategory] = useState("");
+
   return (
     <div>
-      <Header />
+      {/* Pass data to header */}
+      <Header
+        onCategorySelect={setSelectedCategory}
+        onSubCategorySelect={setSelectedSubCategory}
+      />
+
+      <ProductGrid
+        selectedCategory={selectedCategory}
+        selectedSubCategory={selectedSubCategory}
+      />
     </div>
   );
 }
