919fb023ba5e18a16610b56145f9da56dcd1df32
priyans619 <priyansh.2b@gmail.com>
2024-11-21 22:33:46 +0530

    text-color

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -2,17 +2,17 @@ import React from "react";
 
 const ProductCard = ({ product }) => {
   return (
-    <div className="bg-white shadow-lg rounded-lg p-4">
+    <div className="bg-black ">
       <img
         src={product.image}
         alt={product.name}
-        className="w-full h-48 object-cover rounded-md"
+        className="w-full h-48 object-cover object-top rounded-md"
       />
       <div className="mt-4">
-        <h3 className="font-bold text-lg">{product.name}</h3>
-        <p className="text-gray-600">Creator: {product.creator}</p>
-        <p className="text-yellow-500">Rating: {product.rating} ⭐</p>
-        <p className="text-green-600 font-bold">{product.price}</p>
+        <h3 className="font-bold text-lg text-white">{product.name}</h3>
+        <p className="text-white">Creator: {product.creator}</p>
+        <p className="text-yellow-500">Rating ⭐ : {product.ratings} </p>
+        <p className="text-white font-bold">Price: {product.price}$</p>
       </div>
     </div>
   );
